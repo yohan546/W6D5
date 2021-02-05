a6a05e7a8149518004599195e1182839eb68f50d
Lane Harris <lrharris215@gmail.com>
2021-02-05 15:45:58 -0500

    made new form

--- cats_99/app/controllers/cats_controller.rb
+++ cats_99/app/controllers/cats_controller.rb
@@ -10,7 +10,21 @@ class CatsController < ApplicationController
         render 
     end
 
+    def create
+        kitty = Cat.new(cat_params)
+        if kitty.save
+            redirect_to cat_url(kitty)
+        else
+            render json: kitty.errors.full_messages, status: 422
+        end
+    end
+
+    def new
+        @cat = Cat.new
+        render :new
+    end
+
     def cat_params
-        params.require(:cat).permit(:id)
+        params.require(:cat).permit(:id, :name, :birth_date, :color, :sex, :description)
     end
 end
